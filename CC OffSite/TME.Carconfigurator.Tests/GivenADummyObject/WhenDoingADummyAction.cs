3ac5758428ef876e31cefaaec53f4be8dba53538
Tom Knapen <tom.knapen@outlook.com>
2014-09-30 15:50:43 +0200

    Added dummy test

---
+++ CC OffSite/TME.Carconfigurator.Tests/GivenADummyObject/WhenDoingADummyAction.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace TME.Carconfigurator.Tests.GivenADummyObject
+{
+    public class WhenDoingADummyAction
+    {
+        private readonly int _numberOfTestsExecuted;
+
+        public WhenDoingADummyAction()
+        {
+            _numberOfTestsExecuted += 1;
+        }
+
+        [Fact]
+        public void ThenItShouldDoADummyAssert()
+        {
+            _numberOfTestsExecuted.Should().Be(1);
+        }
+    }
+}
