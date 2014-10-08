6445b9979c36fc831b839ae2ce814c35373277dc
Wim De Ceuster <wimdeceuster@yahoo.com>
2014-10-08 15:02:46 +0200

    Changed FrontEndViewer

---
+++ CC OffSite/TME.FrontEndViewer/Models/CompareView.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace TME.FrontEndViewer.Models
+{
+    public class CompareView<T>
+    {
+        public IList<T> OldReaderModel { get; set; }
+        public IList<T> NewReaderModel { get; set; }
+    }
+}
