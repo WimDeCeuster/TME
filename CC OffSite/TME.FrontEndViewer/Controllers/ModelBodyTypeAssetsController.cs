6c2f2dd200bc4d97c3c8ada982fcd7ec668bb07a
Wim De Ceuster <wimdeceuster@yahoo.com>
2014-10-13 12:13:47 +0200

    Added ModelBodyTypes and ModelBodyTypeAssets to Front End Viewer

---
+++ CC OffSite/TME.FrontEndViewer/Controllers/ModelBodyTypeAssetsController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using TME.CarConfigurator.Interfaces;
+using TME.CarConfigurator.Interfaces.Assets;
+using TME.FrontEndViewer.Models;
+using TMME.CarConfigurator;
+using TME.CarConfigurator.Repository.Objects;
+
+
+
+namespace TME.FrontEndViewer.Controllers
+{
+    public class ModelBodyTypeAssetsController : Controller
+    {
+        public ActionResult Index(Guid modelID, Guid bodyTypeID)
+        {
+
+            var context = (Context)Session["context"];
+            var oldContext = MyContext.NewContext(context.Brand, context.Country, context.Language);
+
+            var model = new CompareView<IAsset>
+            {
+                OldReaderModel = GetOldReaderModelWithMetrics(oldContext, modelID, bodyTypeID),
+                NewReaderModel = GetNewReaderModelWithMetrics(context, modelID, bodyTypeID)
+            };
+
+            return View(model);
+        }
+
+        private static ModelWithMetrics<IAsset> GetOldReaderModelWithMetrics(MyContext oldContext, Guid modelID, Guid bodyTypeID)
+        {
+            var start = DateTime.Now;
+            var list = TMME.CarConfigurator.Model.GetModel(oldContext, modelID)
+                            .BodyTypes[bodyTypeID]
+                            .Assets
+                            .Cast<TMME.CarConfigurator.Asset>()
+                            .Select(x => new CarConfigurator.LegacyAdapter.Asset(x))
+                            .ToList();
+
+            return new ModelWithMetrics<IAsset>()
+            {
+                Model = list,
+                TimeToLoad = DateTime.Now.Subtract(start)
+            };
+        }
+        private static ModelWithMetrics<IAsset> GetNewReaderModelWithMetrics(Context context, Guid modelID, Guid bodyTypeID)
+        {
+            var start = DateTime.Now;
+            var list = CarConfigurator.DI.Models
+                .GetModels(context).First(x => x.ID == modelID)
+                .BodyTypes.First(x=> x.ID == bodyTypeID)
+                .Assets;
+
+            return new ModelWithMetrics<IAsset>()
+            {
+                Model = list,
+                TimeToLoad = DateTime.Now.Subtract(start)
+            };
+        }
+
+    }
+}
