dd9ec49f88639032dc2a7bf76bb265ec3429b76c
Ruben Goris <ruben.goris@cronos.be>
2014-10-15 16:33:22 +0200

    Removed last datacontract/datamember properties from repository objects

--- CC OffSite/TME.CarConfigurator.Repository.Objects/CarConfiguratorVersion.cs
+++ CC OffSite/TME.CarConfigurator.Repository.Objects/CarConfiguratorVersion.cs
@@ -2,12 +2,10 @@
 
 namespace TME.CarConfigurator.Repository.Objects
 {
-    [DataContract]
+
     public class CarConfiguratorVersion
     {
-        [DataMember]
         public short ID { get; set; }
-        [DataMember]
         public string Name { get; set; }
     }
 }
