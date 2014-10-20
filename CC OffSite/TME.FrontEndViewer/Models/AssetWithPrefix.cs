a4fa62708e5d76a832fbccd5363b70d73797c082
Wim De Ceuster <wimdeceuster@yahoo.com>
2014-10-20 06:20:17 +0200

    Work in progress on VisibleInModeAndView Front End viewer

---
+++ CC OffSite/TME.FrontEndViewer/Models/AssetWithPrefix.cs
@@ -0,0 +1,11 @@
+﻿using TME.CarConfigurator.Interfaces.Assets;
+
+namespace TME.FrontEndViewer.Models
+{
+    public class AssetWithPrefix
+    {
+        public IAsset Asset { get; set; }
+        public string Prefix { get; set; }
+
+    }
+}
