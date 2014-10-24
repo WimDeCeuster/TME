49dcf46529cfdcd1fdd47ec27fd890149ed311a9
Tom Knapen <tom.knapen@outlook.com>
2014-10-24 15:59:51 +0200

    quick update in shellVM

--- CC OffSite/TME.CarConfigurator.Publisher.UI/ViewModels/ShellViewModel.cs
+++ CC OffSite/TME.CarConfigurator.Publisher.UI/ViewModels/ShellViewModel.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Windows.Threading;
 using Caliburn.Micro;
 using TME.CarConfigurator.Administration;
 using TME.CarConfigurator.Publisher.Common.Enums;
