312fde8f74c31752be82b6561c1612e51002e3f4
Wim De Ceuster <wimdeceuster@yahoo.com>
2014-10-25 22:09:55 +0200

    Fixed various issues related to assets in Front End Viewer, Legacy Adapter & Extensions +preparation in Front End Viewer of Car Transmission and Car WheelDrive assets

--- CC OffSite/TME.FrontEndViewer/Controllers/ModelEngineVisibleInAssetsController.cs
+++ CC OffSite/TME.FrontEndViewer/Controllers/ModelEngineVisibleInAssetsController.cs
@@ -14,56 +14,56 @@ namespace TME.FrontEndViewer.Controllers
 {
     public class ModelEngineVisibleInAssetsController : Controller
     {
-        public ActionResult Index(Guid modelID, Guid engineID, string mode, string view)
+        public ActionResult Index(Guid modelID, Guid? engineID, Guid? carID, string mode, string view)
         {
 
             var context = (Context)Session["context"];
             var oldContext = MyContext.NewContext(context.Brand, context.Country, context.Language);
-
+            
             var model = new CompareView<IAsset>
             {
-                OldReaderModel = GetOldReaderModelWithMetrics(oldContext, modelID, engineID, mode, view),
-                NewReaderModel = GetNewReaderModelWithMetrics(context, modelID, engineID, mode, view)
+                OldReaderModel = GetOldReaderModelWithMetrics(oldContext, modelID, engineID,carID, mode, view),
+                NewReaderModel = GetNewReaderModelWithMetrics(context, modelID, engineID, carID,mode, view)
             };
 
             return View("Assets/Index", model);
         }
 
-        private static ModelWithMetrics<IAsset> GetOldReaderModelWithMetrics(MyContext oldContext, Guid modelID, Guid engineID, string mode, string view)
+        private static ModelWithMetrics<IAsset> GetOldReaderModelWithMetrics(MyContext oldContext, Guid modelID, Guid? engineID, Guid? carID, string mode, string view)
         {
             var start = DateTime.Now;
+            var model = new CarConfigurator.LegacyAdapter.Model(TMME.CarConfigurator.Model.GetModel(oldContext, modelID));
+            var list = GetList(model, engineID, carID, mode, view);
 
-            var engine = new CarConfigurator.LegacyAdapter.Engine(
-                TMME.CarConfigurator.Model.GetModel(oldContext, modelID)
-                .Engines.Cast<TMME.CarConfigurator.Engine>()
-                .First(x => x.ID == engineID));
-
-            var visibleIn = engine.VisibleIn.FirstOrDefault(x => x.Mode == (mode ?? "") && x.View == (view ?? ""));
-            var list = visibleIn == null
-                ? new List<IAsset>()
-                : visibleIn.Assets.ToList();
             return new ModelWithMetrics<IAsset>()
             {
                 Model = list,
                 TimeToLoad = DateTime.Now.Subtract(start)
             };
         }
-        private static ModelWithMetrics<IAsset> GetNewReaderModelWithMetrics(Context context, Guid modelID, Guid engineID, string mode, string view)
+        private static ModelWithMetrics<IAsset> GetNewReaderModelWithMetrics(Context context, Guid modelID, Guid? engineID, Guid? carID, string mode, string view)
         {
             var start = DateTime.Now;
-            var engine = CarConfigurator.DI.Models
-                .GetModels(context).First(x => x.ID == modelID)
-                .Engines.First(x => x.ID == engineID);
+            var model = CarConfigurator.DI.Models.GetModels(context).First(x => x.ID == modelID);
+            var list = GetList(model, engineID, carID, mode, view);
 
-            var visibleIn = engine.VisibleIn.FirstOrDefault(x => x.Mode == (mode ?? "") && x.View == (view ?? ""));
-            var list = visibleIn == null
-                ? new List<IAsset>()
-                : visibleIn.Assets.ToList();
             return new ModelWithMetrics<IAsset>()
             {
                 Model = list,
                 TimeToLoad = DateTime.Now.Subtract(start)
             };
+        }
+        private static List<IAsset> GetList(IModel model, Guid? engineID, Guid? carID, string mode, string view)
+        {
+            var engine = (carID == null
+                ? model.Engines.First(x => x.ID == engineID.Value)
+                : model.Cars.First(x => x.ID == carID.Value).Engine);
+
+            var visibleIn = engine.VisibleIn.FirstOrDefault(x => x.Mode == mode && x.View == view);
+            var list = visibleIn == null
+                ? new List<IAsset>()
+                : visibleIn.Assets.ToList();
+            return list;
         }
 
     }
