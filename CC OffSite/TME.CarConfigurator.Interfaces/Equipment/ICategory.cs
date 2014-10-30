3b237a3ebeeef57248bd8a3078418f0a4633e491
Wim De Ceuster <wimdeceuster@yahoo.com>
2014-10-30 07:02:42 +0100

    Introduced Equipment Category interface and repo object

---
+++ CC OffSite/TME.CarConfigurator.Interfaces/Equipment/ICategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using TME.CarConfigurator.Interfaces.Core;
+
+namespace TME.CarConfigurator.Interfaces.Equipment
+{
+    public interface ICategory : IBaseObject
+    {
+        String Path { get; }
+        ICategory Parent { get; }
+        IReadOnlyList<ICategory> Categories { get; }
+    }
+}
