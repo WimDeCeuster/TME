e5e3b18abe7a9ec100b0b0d55b1846dc3f86a117
Ruben Goris <ruben.goris@cronos.be>
2014-10-31 12:19:18 +0100

    equipment categories key

--- CC OffSite/TME.CarConfigurator.S3.Shared/Interfaces/IKeyManager.cs
+++ CC OffSite/TME.CarConfigurator.S3.Shared/Interfaces/IKeyManager.cs
@@ -22,5 +22,6 @@ namespace TME.CarConfigurator.S3.Shared.Interfaces
         String GetAssetsKey(Guid publicationID, Guid objectID, String view, String mode);
         String GetAssetsKey(Guid publicationID, Guid carID, Guid objectID, String view, String mode);
         String GetSubModelGradeEquipmentsKey(Guid publicationID, Guid timeFrameID, Guid gradeID, Guid subModelID);
+        String GetEquipmentCategoriesKey(Guid publicationID, Guid timeFrameID);
     }
 }
