310ae38cc3ac7e2db9bb69495ac097c741d04c11
YensProost <yens.proost@gmail.com>
2014-12-02 11:03:56 +0100

    BuildFailFix

---
+++ CC OffSite/TME.CarConfigurator.QueryRepository.Tests/GivenAnEquipmentRule/WhenAccessingItsCategoryForTheFirstTime.cs
@@ -0,0 +1,20 @@
+﻿using TME.CarConfigurator.Interfaces.Rules;
+using TME.CarConfigurator.Tests.Shared;
+
+namespace TME.CarConfigurator.Query.Tests.GivenAnEquipmentRule
+{
+    public class WhenAccessingItsCategoryForTheFirstTime : TestBase
+    {
+        private IEquipmentRule _equipmentRule;
+
+        protected override void Arrange()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void Act()
+        {
+            _equipmentRuleCategory = _equipmentRule.Category;
+        }
+    }
+}
